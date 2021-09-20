b650e9c19b8897e43bb93d9dafe29ec29a2a524a
Khaled Lela <eng.khaled.lela@gmail.com>
2021-09-20 23:52:20 +0200

    update recursive max

--- chapter_4/max_recursive.py
+++ chapter_4/max_recursive.py
@@ -1,8 +1,12 @@
 
 def findMax(arr):
     if (len(arr) == 0):
-        return arr[0]
-    return max(arr[len(arr) - 1], findMax(arr[1:]))
+        return 0
+    # Use Python built in max function or check it.
+    sub_max = findMax(arr[1:])
+    el = arr[0]
+    return el if el > sub_max else sub_max
+    # return max(arr[len(arr)-1], findMax(arr[1:]))
 
 
-print(max([5, 2, 6, 10, 3]))
+print(findMax([5, 2, 6, 10, 3]))
